ebbc962b522372aaec9a70902301fcee5cfaa5fd
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-02-21 20:38:01 -0300

    instala o bando nedb

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,6 @@
+const nedb = require("nedb");
+const db = new nedb({ filename: "users.db", autoload: true });
+
 module.exports = (app) => {
   app.get("/users", (req, res) => {
     res.statusCode = 200;
@@ -19,6 +22,15 @@ module.exports = (app) => {
   });
 
   app.post("/users", (req, res) => {
-    res.json(req.body);
+    db.insert(req.body, (err, user) => {
+      if (err) {
+        console.log("error:" + err);
+        res.status(400).json({
+          error: err,
+        });
+      } else {
+        res.status(200).json(user);
+      }
+    });
   });
 };
