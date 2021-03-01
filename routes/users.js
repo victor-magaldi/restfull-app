bdc1dcbb9ccfbb543c879737a7426819c88f7467
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-03-01 20:34:11 -0300

    ajusta as requests get e post

--- routes/users.js
+++ routes/users.js
@@ -2,32 +2,28 @@ const nedb = require("nedb");
 const db = new nedb({ filename: "users.db", autoload: true });
 
 module.exports = (app) => {
-  app.get("/users", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.json({
-      users: [
-        {
-          name: "Victor de Souza Magalid",
-          idade: 27,
-          email: "victmagaldi@hotmail.com",
-        },
-        {
-          name: "João Silva",
-          idade: 99,
-          email: "teste@hotmail.com",
-        },
-      ],
-    });
+  let route = app.route("/users");
+
+  route.get((req, res) => {
+    db.find({})
+      .sort({ name: 1 })
+      .exec((err, users) => {
+        if (err) {
+          route.utils.error.send(err, req, res);
+        } else {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json({
+            users,
+          });
+        }
+      });
   });
 
-  app.post("/users", (req, res) => {
+  route.post((req, res) => {
     db.insert(req.body, (err, user) => {
       if (err) {
-        console.log("error:" + err);
-        res.status(400).json({
-          error: err,
-        });
+        app.utils.error.send(err, req, res);
       } else {
         res.status(200).json(user);
       }
